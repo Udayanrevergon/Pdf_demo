83d7aa950b8a1ba67cf301f982d40e1d858f86eb
udayanvats <udayan.asus06@gmail.com>
2023-06-14 17:34:16 +0530

    Modified images

--- lib/PdfApi.dart
+++ lib/PdfApi.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/widgets.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import 'package:rev_pdf/save_launch.dart';
@@ -14,68 +16,77 @@ class PageoneData {
 }
 
 class PdfApi {
-  // static Future<Uint8List> createDocument() {
-  //   final pdf = pw.Document();
-  //   pdf.addPage(pw.Page(build: (pw.Context context) {
-  //     return pw.Center(child: pw.Text("Hello World"));
-  //   }));
-  //   return pdf.save();
-  // }
-  // static Future<File> createDocument() async {
-  //   final pdf = pw.Document();
-  //   final font = await PdfGoogleFonts.nunitoExtraLight();
-  //   final cellStyle =
-  //       pw.TextStyle(font: font, fontSize: 12, fontWeight: pw.FontWeight.bold);
-
-  //   final headers = ['Name', 'Age'];
-
-  //   final users = [
-  //     PageoneData(attribute: 'Organisation', value: 'Infosys'),
-  //     PageoneData(attribute: 'Date', value: 'Infosys'),
-  //     PageoneData(attribute: 'Place', value: 'Infosys'),
-  //   ];
-  //   final data = users.map((user) => [user.attribute, user.value]).toList();
-
-  //   pdf.addPage(pw.Page(
-  //     build: (context) => pw.Table.fromTextArray(
-  //         headers: headers, data: data, cellStyle: cellStyle),
-  //   ));
-//   Future<void> createPDF() async {
-//   //Create a new PDF document
-//   PdfDocument document = PdfDocument();
-//   PdfPage page = document.pages.add();
-
-//   //Add a new page and draw text
-//   // page.graphics.drawString(
-//   //     'Hello World!', PdfStandardFont(PdfFontFamily.helvetica, 20),
-//   //     brush: PdfSolidBrush(PdfColor(0, 0, 0)),
-//   //     bounds: Rect.fromLTWH(0, 0, 500, 50));
-//      page.graphics.drawImage(
-//     PdfBitmap(File('assets/images/image.jpeg').readAsBytesSync()),
-//     Rect.fromLTWH(
-//         0, 0, page.getClientSize().width, page.getClientSize().height));
-
-// //Saves the document
-// File('Output.pdf').writeAsBytes(await document.save());
-
-//   //Save the document
-//   List<int> bytes = await document.save();
-
-//   //Dispose the document
-//   document.dispose();
-//   saveAndLaunchFile(bytes, 'Output.pdf');
-// }
-  //   return saveDocument(name: 'my_example.pdf', pdf: pdf);
-  // }
   static Future<File> createDocument() async {
     print("I am here3");
     // final imageSvg = await rootBundle.loadString('assets/images/image1.svg');
-    final imagejpeg = (await rootBundle.load('assets/images/image.jpeg')).buffer.asUint8List();
+    final imagejpeg = (await rootBundle.load('assets/images/image.jpeg'))
+        .buffer
+        .asUint8List();
+    final centreImage =
+        (await rootBundle.load('assets/images/revergon_image.jpeg'))
+            .buffer
+            .asUint8List();
 
     final pdf = pw.Document();
-    pdf.addPage(pw.MultiPage(build: (context) => [
-      pw.Image(pw.MemoryImage(imagejpeg))
-      ]));
+    pdf.addPage(pw.MultiPage(
+        pageTheme: pw.PageTheme(margin: pw.EdgeInsets.all(0)),
+        build: (context) => [
+              pw.Container(
+                  width: double.infinity,
+                  height: 840,
+                  child: pw.Stack(alignment: pw.Alignment.center, children: [
+                    pw.Positioned(
+                      top: 5,
+                      child: pw.Image(pw.MemoryImage(imagejpeg),
+                          height: 800, width: 600),
+                    ),
+                    pw.Positioned(
+                      bottom: 450,
+                      child: pw.Image(pw.MemoryImage(centreImage),
+                          height: 350, width: 400),
+                    ),
+                    pw.Positioned(
+                      bottom: 415,
+                      child: pw.Row(
+                        children: [
+                          pw.Text("WORKSTATION ",
+                              style: pw.TextStyle(
+                                color: PdfColors.grey700,
+                                fontSize: 20,
+                                fontWeight: pw.FontWeight.bold,
+                              )),
+                          pw.Text("Assessment ",
+                              style: pw.TextStyle(
+                                  fontSize: 20, color: PdfColors.grey700)),
+                          pw.Text("Report ",
+                              style: pw.TextStyle(
+                                  fontSize: 20, color: PdfColors.grey700)),
+                        ],
+                      ),
+                    ),
+                    pw.Positioned(
+                      bottom: 300,
+                      child: pw.Container(
+                        width: 300,
+                        height: 500,
+                        child: pw.TableHelper.fromTextArray(
+                          headers: [],
+                          cellStyle: const pw.TextStyle(fontSize: 12),
+                          cellAlignment: pw.Alignment.topLeft,
+                          context: context,
+                          data: const <List<String>>[
+                            // <String>['Date', 'PDF Version', 'Acrobat Version'],
+                            <String>['Patient Name', 'TTTTT'],
+                            <String>['Organization', 'IBM'],
+                            <String>['Date', '14-06-23'],
+                            <String>['Place', 'Bangalore'],
+                            // <String>['1999', 'PDF 1.3'],
+                          ],
+                        ),
+                      ),
+                    ),
+                  ])),
+            ]));
     return saveDocument(name: 'my_example.pdf', pdf: pdf);
   }
 }
